e93a18f63999189d54128799e8b698a6768835bd
Matias <matiassanchez13422@gmail.com>
2024-11-19 11:11:25 -0300

    agregamos el codigo para hacer funcionar el modal

--- js/app.js
+++ js/app.js
@@ -2,7 +2,21 @@
 // si exporto mas de una cosa lo que quiero importar va con {contacto, url}
 import Contacto from "./classContacto.js"
 
-
+// declaro variables globales
 const nuevoContacto = new Contacto('Juan', 'Perez', '3815646882','juanpereaz@gmail.com', 'Juancho', 'http://127.0.0.1:5500/Index.html?')
 console.log(nuevoContacto)
 
+const modalContacto = new bootstrap.Modal(document.querySelector('#Modal'));
+const btnAgregar = document.querySelector('#BotonAgregar')
+
+
+
+console.log(btnAgregar)
+// funciones
+function abrirModal(){
+    
+modalContacto.show()
+}
+
+// el resto de la logica del proyexto
+btnAgregar.addEventListener('click', abrirModal)

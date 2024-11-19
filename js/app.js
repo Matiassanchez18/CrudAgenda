0f2f8a8cb2732a8b9c5dfe7cc3df4e81303c4db2
Matias <matiassanchez13422@gmail.com>
2024-11-19 10:42:02 -0300

    Se agrego una clase persona

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+
+// si exporto mas de una cosa lo que quiero importar va con {contacto, url}
 import Contacto from "./classContacto.js"
 
 
 const nuevoContacto = new Contacto('Juan', 'Perez', '3815646882','juanpereaz@gmail.com', 'Juancho', 'http://127.0.0.1:5500/Index.html?')
 console.log(nuevoContacto)
+
--- js/classContacto.js
+++ js/classContacto.js
@@ -1,3 +1,6 @@
+// Para exportar una sola cosa se usa export default, pero cuando quiero exportar mas de 1 se usa export y se le agrega a cada cosa que quiero exportar 
+// export const url = 'http://127.0.0.1:5500/Index.html?'
+
 export default class Contacto {
 
     #id

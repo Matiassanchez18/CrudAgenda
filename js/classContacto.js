2deb17df7854f7f96c1b5a6056d4f752f9d08780
Matias <matiassanchez13422@gmail.com>
2024-11-21 09:26:21 -0300

    Se agrego los set y get

--- js/classContacto.js
+++ js/classContacto.js
@@ -23,4 +23,58 @@ export default class Contacto {
 
     }
     // agregar get y set
+    get id(){
+        return this.#id
+    }
+
+    set id(nuevoID){
+   this.#id = nuevoID
+    }
+
+
+    get nombre(){
+        return this.#nombre
+    }
+
+    set nombre(nuevoNombre){
+        this.#nombre = nuevoNombre
+    }
+
+
+    get apellido(){
+        return this.#apellido
+    }
+    set apellido(nuevoApellido){
+        this.#apellido = nuevoApellido
+    }
+
+    get telefono(){
+        return this.#telefono
+    }
+    set telefono(nuevotelefono){
+        this.#telefono = nuevotelefono
+    }
+
+    get mail(){
+        return this.#mail
+    }
+    set mail(nuevomail){
+        this.#mail = nuevomail
+    }
+
+    get apodo(){
+        return this.#apodo
+    }
+    set apodo(nuevoapodo){
+        this.#apodo = nuevoapodo
+    }
+
+    get url(){
+        return this.#url
+    }
+    set url(nuevourl){
+        this.#url = nuevourl
+    }
+
+  
 }
